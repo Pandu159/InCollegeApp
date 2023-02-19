619b0c8e706e9af33194160f9d34f86235e46894
Pandu159 <54086442+Pandu159@users.noreply.github.com>
2023-02-19 01:52:16 -0500

    Epic 3

--- feed.py
+++ feed.py
@@ -1,12 +1,237 @@
 from authentication import *
 
 
-def HomeScreen():
+def mainPage():
+    print("Input 9 to view a video that would explain why joining InCollege would be highly beneficial for you")
+    print("Input 2 to explore Useful Links or Input 3 to explore InCollege Important Links")
+    selection = int(input("To create an account, input 0, to sign in input 1: "))
+
+    userName = None
+
+    if selection == 0:
+        userName = signUp()
+    elif selection == 1:
+        userName = signIn()
+    elif selection == 9:
+        print("Video is now playing!")
+        mainPage()
+    elif selection == 2:
+        selectUsefulLinks(0, None)
+    elif selection == 3:  # InCollegeImportant
+        selectInCollegeImportant(0, None)
+
+    else:
+        print("Invalid Selection!")
+        exit(-1)
+
+    if userName is not None:
+        selectOption(userName)
+
+
+def homeScreen():
     print("Welcome to InCollege!")
     print("Meet Jake, a recent college grad in marketing who found out about InCollege at a career fair.")
-    print(
-        "After creating his profile, connecting with industry professionals, and joining related groups, Jake received a message from a recruiter on InCollege, who was impressed by his profile and wanted to interview him.")
+    print("After creating his profile, connecting with industry professionals, and joining related groups, Jake received a message from a recruiter on InCollege, who was impressed by his profile and wanted to interview him.")
     print("He aced the interview and got the job on the spot. Jake's success shows the value of using InCollege.")
+
+
+def selectUsefulLinks(i, uName):
+
+    link = int(input("Please select a skill to learn from the list:\n1-General\n2-Browse InCollege\n3-Business Solutions\n4-Directories\n5-Return to previous screen\n"))
+
+    m = i
+
+    if link == 1:
+        selectGeneral(m, uName)
+    elif link == 2:
+        print("under construction")
+        selectUsefulLinks(m, uName)
+    elif link == 3:
+        print("under construction")
+        selectUsefulLinks(m, uName)
+    elif link == 4:
+        print("under construction")
+        selectUsefulLinks(m, uName)
+    elif link == 5:
+        if(i == 0):
+            mainPage()
+        else:
+            selectOption(uName)
+
+    else:
+        print("Not a valid option")
+        exit(-1)
+
+
+def selectInCollegeImportant(i, uName):
+
+    link = int(input("Please select a link from the following:\n 1 - Copyright Notice\n 2 - About\n 3 - Accessibility\n 4 - User Agreement\n 5 - Privacy Policy\n "
+                     "6 - Cookie Policy\n 7 - Brand Policy\n 8 - Languages\n 9 - Return to previous screen\n"))
+    # Copyright Notice
+    if link == 1:
+        print("Copyright Notice: Developers of InCollege App")
+        # Return to options
+        select = int(input("0 - Return to previous screen: "))
+        if select == 0:
+            selectInCollegeImportant(1, uName)
+
+    # About
+    elif link == 2:
+        print("About screen")
+        # Return to options
+        select = int(input("0 - Return to previous screen: "))
+        if select == 0:
+            selectInCollegeImportant(1, uName)
+
+    # Accessibility
+    elif link == 3:
+        print("Accessibility options")
+        # Return to options
+        select = int(input("0 - Return to previous screen: "))
+        if select == 0:
+            selectInCollegeImportant(1, uName)
+
+    # User Agreement
+    elif link == 4:
+        print("User Agreement = yes")
+        # Return to options
+        select = int(input("0 - Return to previous screen: "))
+        if select == 0:
+            selectInCollegeImportant(1, uName)
+
+    # Privacy Policy
+    elif link == 5:
+        # A signed-in user will have the option to edit their Guest Controls
+        if uName is not None:
+            print(f"Welcome {uName}!\n")
+            temp = input("Would you like to edit your Guest Controls? (Y/N)")
+            if temp == "Y":
+                guestControls(uName)
+        # Privacy Policy is listed
+        print("Privacy Policy")
+        # Return to options
+        select = int(input("0 - Return to previous screen: "))
+        if select == 0:
+            selectInCollegeImportant(1, uName)
+
+    # Cookie Policy
+    elif link == 6:
+        print("Cookie Policy")
+        select = int(input("0 - Return to previous screen: "))
+        if select == 0:
+            selectInCollegeImportant(1, uName)
+
+    # Brand Policy
+    elif link == 7:
+        print("Brand Policy")
+        # Return to options
+        select = int(input("0 - Return to previous screen: "))
+        if select == 0:
+            selectInCollegeImportant(1, uName)
+
+    # Languages
+    elif link == 8:
+        if uName is not None:
+            lang = input("Languages: English or Spanish? (E/S)")
+            if lang == "E":
+                print("Language switched to English")
+                # update user json to english
+                updateUserInfo(uName, "language", "English")
+            elif lang == "S":
+                print("Spanish Language not available yet")
+                # update user json to spanish
+                updateUserInfo(uName, "language", "Spanish")
+            # Return to options
+            select = int(input("0 - Return to previous screen: "))
+            if select == 0:
+                selectInCollegeImportant(1, uName)
+
+    # Return to previous screen
+    elif link == 9:
+        print("Returning to previous screen...")
+        if i == 0:
+            mainPage()
+        else:
+            selectOption(uName)
+
+    # Invalid option
+    else:
+        print("Invalid option... Showing options again...")
+        selectInCollegeImportant(1, uName)
+
+
+def guestControls(uName):
+    select = int(input("Which setting would you like to turn off: 1 - InCollege Email, 2 - SMS, "
+                       "3 - Targeted Advertising, 9 - Return to previous menu\n"))
+    if select == 1:
+        updateUserInfo(uName, "inCollegeEmail", "off")
+        print("InCollege Email successfully turned off, returning to previous menu...")
+    elif select == 2:
+        updateUserInfo(uName, "SMS", "off")
+        print("SMS successfully turned off, returning to previous menu...")
+    elif select == 3:
+        updateUserInfo(uName, "targetedAds", "off")
+        print("Targeted Advertising successfully turned off, returning to previous menu...")
+    elif select == 9:
+        print("Returning to previous menu...")
+        return
+    else:
+        print("Invalid option, returning to previous menu...")
+        return
+
+
+def selectGeneral(i,Uname):
+    selection = int(input("Please select from the following options:\n1 - SignUp\n""2 - Help Center\n3 - About\n""4 - Press \n5 - Blog\n""6 - Careers\n7 - Developers\n""8 - Return to previous screen\n"))
+    m = i
+
+    userName = Uname
+    if selection == 1:
+        if userName is None:
+            userName = signUp()
+            select = int(input("0 - Return to previous screen: "))
+            if select == 0:
+                selectGeneral(m, userName)
+        else:
+            print("Already Signed In")
+            select = int(input("0 - Return to previous screen: "))
+            if select == 0:
+                selectGeneral(m, userName)
+    elif selection == 2:
+        print("We're here to help")
+        select = int(input("0 - Return to previous screen: "))
+        if select == 0:
+            selectGeneral(m, userName)
+    elif selection == 3:
+        print("In College: Welcome to In College, the world's largest college student network with many users in many"
+              " countries and territories worldwide.")
+        select = int(input("0 - Return to previous screen: "))
+        if select == 0:
+            selectGeneral(m, userName)
+    elif selection == 4:
+        print("In College Pressroom: Stay on top of the latest news, updates, and reports")
+        select = int(input("0 - Return to previous screen\n"))
+        if select == 0:
+            selectGeneral(m, userName)
+    elif selection == 5:
+        print("Under construction")
+        select = int(input("0 - Return to previous screen\n"))
+        if select == 0:
+            selectGeneral(m, userName)
+    elif selection == 6:
+        print("Under construction")
+        select = int(input("0 - Return to previous screen\n"))
+        if select == 0:
+            selectGeneral(m, userName)
+    elif selection == 7:
+        print("Under construction")
+        select = int(input("0 - Return to previous screen\n"))
+        if select == 0:
+            selectGeneral(m, userName)
+    elif selection == 8:
+        selectUsefulLinks(m, userName)
+    else:
+        print("Not a valid option")
+        exit(-1)
 
 
 # this function lists the (up to) 5 current job listings and offers the option to add more if there is room
@@ -15,7 +240,7 @@ def findJob(uName):
     printJobs()
 
     # gets the dictionary of jobs and counts how many there are into numJobs
-    jobs = getJobs()
+    jobs = getJson("jobs")
     numJobs = len(jobs)
 
     # if there are 4 or less jobs, it offers the user to add another job
@@ -42,11 +267,16 @@ def createJob(uName):
     location = input("Please enter a location: ")
     salary = float(input("Please enter a salary: "))
 
-    # gets the dictionary of jobs and appends the new job
-    jobs = getJobs()
+    # gets the dictionary of jobs and name from the userName
+    # then appends to json file
+    jobs = getJson("jobs")
+    users = readUsers()
+    for user in users:
+        if uName == user["username"]:
+            name = user["firstName"] + " " + user["lastName"]
     jobs.append(
         {"title": title, "description": description, "employer": employer, "location": location, "salary": salary,
-         "Name": uName})
+         "Name": name})
     with open("jobs.json", "w") as f:
         json.dump(jobs, f)
 
@@ -56,7 +286,7 @@ def createJob(uName):
 # this function prints the list of jobs in jobs.json
 def printJobs():
     # gets the dictionary of jobs
-    jobs = getJobs()
+    jobs = getJson("jobs")
 
     # for loop iterates through all jobs
     i = 0
@@ -75,14 +305,15 @@ def printJobs():
         print(f'Salary: {jobDesc["salary"]}')
         print("\n")
 
-        # increments the counter variable
+        # iterate counter i
         i += 1
 
 
 # this function opens the jobs.json file and returns a dictionary of values
-def getJobs():
+# EDIT this function now opens multiple json files a returns a dictionary of values
+def getJson(fileName):
     try:
-        with open("jobs.json", "r") as f:
+        with open(fileName+".json", "r") as f:
             return json.load(f)
     except FileNotFoundError:
         return []
@@ -105,29 +336,42 @@ def findPeople():
     return False
 
 
-def selectSkill():
-    skill = input(
-        "Please select a skill to learn from the list:\n1-Time management\n2-Teamwork\n3-Written communication\n4-Verbal communication\n5-Project management\n6-Return to main menu\n")
+def selectSkill(uName):
+    skill = int(input("Please select a skill to learn from the list:\n1-Time management\n2-Teamwork\n3-Written communication\n4-Verbal communication\n5-Project management\n6-Return to Options menu\n"))
     if skill == 1:
         print("under construction")
+        select = int(input("0 - Return to previous screen: "))
+        if select == 0:
+            selectSkill(uName)
     elif skill == 2:
         print("under construction")
+        select = int(input("0 - Return to previous screen: "))
+        if select == 0:
+            selectSkill(uName)
     elif skill == 3:
         print("under construction")
+        select = int(input("0 - Return to previous screen: "))
+        if select == 0:
+            selectSkill(uName)
     elif skill == 4:
         print("under construction")
+        select = int(input("0 - Return to previous screen: "))
+        if select == 0:
+            selectSkill(uName)
     elif skill == 5:
         print("under construction")
+        select = int(input("0 - Return to previous screen: "))
+        if select == 0:
+            selectSkill(uName)
     elif skill == 6:
-        # selectOption()
-        pass
+        selectOption(uName)
     else:
         print("Not a valid option")
         exit(-1)
 
 
 def selectOption(uName):
-    option = int(input("Please select from the following options:\n1 - Find a job/internship\n2 - Find someone you know\n""3 - Learn a new skill\n0 - To exit\n"))
+    option = int(input("Please select from the following options:\n1 - Find a job/internship\n2 - Find someone you know\n""3 - Learn a new skill\n""4 - Useful Links \n5 - InCollege Important Links\n0 - To Log Out\n"))
 
     if option == 1:
         findJob(uName)
@@ -138,10 +382,16 @@ def selectOption(uName):
         else:
             selectOption(uName) # else, it lists the options again
     elif option == 3:
-        selectSkill()
+        selectSkill(uName)
         selectOption(uName)
+    elif option == 4:
+        selectUsefulLinks(1, uName)
+    elif option == 5: # InCollege Important Links
+        selectInCollegeImportant(1, uName)
+
     elif option == 0:
-        return
+        print("User logged out")
+        exit(1)
     else:
         print("Not a valid option")
         exit(-1)
